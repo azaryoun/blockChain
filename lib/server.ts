1c658f0ac09502093fb64dfdbbcb3d143834f4f3
Keiwan Azaryoun <k.azaryoun@outlook.com>
2018-10-30 13:30:32 +0330

    inital commit

---
+++ lib/server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+const port = 4040 || process.env.port;
+app.listen(port, function () {
+    console.log('Express server listening on port ' + port);
+});
+
+//curl 'http://localhost:4040' -method:post -body:id=12,name='keiwan',age=38
